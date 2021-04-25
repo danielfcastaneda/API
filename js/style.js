947636421061e7d87b427876b767a0f56de6353c
dfcastaneda <dfcastaneda200@misena.edu.co>
2021-04-24 20:06:16 -0500

    acceder a los datos

--- js/style.js
+++ js/style.js
@@ -6,11 +6,11 @@ request.open('GET','https://hplussport.com/api/products?qty=3&order=name');
 request.onload = function()
 {
     var response = request.response;
-    console.log(response);
-    console.log(typeof response)
     var jsonData = JSON.parse(response);
     console.log(jsonData);
-    console.log(typeof jsonData); 
+    var descripcion = jsonData[0].description; 
+    console.log(descripcion);
+
 }
 
 request.send();
